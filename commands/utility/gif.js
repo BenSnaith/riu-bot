15a3aebb3527427b93834f01be1da3e284abb691
BenSnaith <bsnaith.bs@icloud.com>
2024-07-25 23:55:06 +0100

    Updated /gif to use the Button message component.

--- commands/utility/gif.js
+++ commands/utility/gif.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ButtonStyle, ActionRowBuilder, ButtonBuilder, awaitMessageComponent } = require('discord.js');
 const list = require('../../gif-list.json');
 
 module.exports = {
@@ -9,6 +9,36 @@ module.exports = {
     async execute(interaction) {
         const randomgif = list[Math.floor(Math.random() * list.length)];
 
-        await interaction.reply(randomgif);
+        const another = new ButtonBuilder()
+            .setCustomId('another-gif')
+            .setLabel('Another?')
+            .setStyle(ButtonStyle.Success);
+
+        const row = new ActionRowBuilder()
+            .addComponents(another);
+
+        const response = await interaction.reply({
+            content: randomgif,
+            components: [row],
+        });
+
+        // Ensure that only the original command caller can respond
+        const collectionFilter = i => i.user.id === interaction.user.id;
+        try {
+            const confirmation = await response.awaitMessageComponent({ filter: collectionFilter, time: 60000});
+
+            if(confirmation.customId === 'another-gif') {
+                const newRandomGif = list[Math.floor(Math.random() * list.length)];
+                // await response.update({
+                //     content: randomgif,
+                //     components: [row],
+                // });
+                interaction.editReply({ content: newRandomGif, components: [row] });
+            }
+        }
+        catch(e) {
+            await interaction.editReply({ content: randomgif, components: [] });
+            await console.log('failed try catch');
+        }
     },
 };
